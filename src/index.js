b14a09b9c51a30241399daf29b72a8969971bddc
Anael961 <98133723+Anael961@users.noreply.github.com>
2022-01-30 17:31:47 -0500

    Updated HTML, CSS, and JS to fetch the correct weather icon from the weather API; updated background color in CSS and added a background image

--- src/index.js
+++ src/index.js
@@ -41,6 +41,12 @@ function showTemperature(response) {
   document.querySelector("#date").innerHTML = formatDate(
     response.data.dt * 1000
   );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 
   celciusTemp = response.data.main.temp;
 }
